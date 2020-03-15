6d044360c8919014169152cb7ef9c4267c2f2214
l3ch4tno1r <arnaud.granier34@gmail.com>
2020-03-16 00:26:19 +0100

    Moving stuffs

---
+++ Source/Geometry/Geometry3D/Transform3D.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Matrix\Stack\SqrSMatrix.h"
+
+#include "HVector3D.h"
+
+using SMatrix44f    = Matrix::StaticMatrix::Matrix<float, 4, 4>;
+using SqrSMatrix44f = Matrix::StaticMatrix::SqrMatrix<float, 4>;
+
+struct Transform3D
+{
+	union
+	{
+		struct
+		{
+			float Rux, Rvx, Rwx, Tx,
+				  Ruy, Rvy, Rwy, Ty,
+				  Ruz, Rvz, Rwz, Tz;
+
+			// Representation of :
+			// [ Rux, Rvx, Rwx, Tx ]
+			// [ Ruy, Rvy, Rwy, Ty ]
+			// [ Ruz, Rvz, Rwz, Tz ]
+			// [  0,   0,   0,   1 ]
+		};
+
+		SqrSMatrix44f mat;
+	};
+
+	Transform3D();
+
+	Transform3D(const Transform3D& _tr);
+
+	Transform3D(const SMatrix44f& _mat);
+
+	Transform3D(const SqrSMatrix44f& _mat);
+};
+
+Transform3D operator*(const Transform3D& a, const Transform3D& b);
+
+HVector3D operator*(const Transform3D& t, const HVector3D& v);
